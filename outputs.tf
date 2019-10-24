3cc8b36c9ac59b59c4595f4c84ee72b99125f0b2
Taras <kindrat5@gmail.com>
2019-10-24 16:56:37 +0300

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "teamcity-ci_external_ip" {
-  value = "${google_compute_instance.teamcity-ci.network_interface.0.access_config.0.nat_ip}"
-}
-
-output "teamcity-ci_internal_ip" {
-  value = "${google_compute_instance.teamcity-ci.network_interface.0.network_ip}"
-}
