087bce91abf6596f03e2cd058acafd7132533bfb
taras <taras@TeamCity>
2019-10-28 13:59:58 +0000

    TeamCity change in 'Demo2' project: runners of 'Build_Carts' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_BuildCarts.kts
+++ .teamcity/patches/buildTypes/Demo2_BuildCarts.kts
@@ -23,7 +23,7 @@ create(RelativeId("Demo2"), BuildType({
 
     steps {
         maven {
-            goals = "clean test package"
+            goals = "clean test"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
         step {
@@ -34,6 +34,15 @@ create(RelativeId("Demo2"), BuildType({
             param("jetbrains.buildServer.deployer.targetUrl", "web")
             param("jetbrains.buildServer.sshexec.authMethod", "CUSTOM_KEY")
             param("jetbrains.buildServer.deployer.ssh.transport", "jetbrains.buildServer.deployer.ssh.transport.scp")
+            param("jetbrains.buildServer.sshexec.keyFile", "/home/taras/.ssh/id_rsa")
+        }
+        step {
+            name = "Stop carts service"
+            type = "ssh-exec-runner"
+            param("jetbrains.buildServer.deployer.username", "taras")
+            param("jetbrains.buildServer.sshexec.command", "sudo systemctl stop carts")
+            param("jetbrains.buildServer.deployer.targetUrl", "web")
+            param("jetbrains.buildServer.sshexec.authMethod", "CUSTOM_KEY")
             param("jetbrains.buildServer.sshexec.keyFile", "/home/taras/.ssh/id_rsa")
         }
     }
