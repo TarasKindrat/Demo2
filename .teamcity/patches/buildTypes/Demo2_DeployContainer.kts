8a171f2e177add14d9bd7460b086d2892cad7ee5
taras <taras@TeamCity>
2019-10-28 13:08:37 +0000

    TeamCity change in 'Demo2' project: runners of 'Deploy_container' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_DeployContainer.kts
+++ .teamcity/patches/buildTypes/Demo2_DeployContainer.kts
@@ -21,7 +21,7 @@ create(RelativeId("Demo2"), BuildType({
     steps {
         script {
             name = "Run mongodb3.4"
-            scriptContent = "rsync -e docker -H ssh://-oStrictHostKeyChecking=no taras@mongo-db run -d mongo:3.4 -p 27017:27017"
+            scriptContent = "docker -H ssh://-oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null taras@mongo-db run -d mongo:3.4 -p 27017:27017"
         }
     }
 }))
