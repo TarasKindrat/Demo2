3323bbbb370c42e96ad950ad8542953482e6d496
TarasKindrat <kindrat5@gmail.com>
2019-10-28 23:27:04 +0200

    modify Dockerfile and add git install to sh script

--- files/web_install.sh
+++ files/web_install.sh
@@ -1,5 +1,12 @@
+echo "install latest git versin"
+sudo yum -y groupinstall "Development Tools";
+sudo yum -y install gettext-devel openssl-devel perl-CPAN perl-devel zlib-devel;
+sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm;
+# remove old version if exist
+sudo yum -y remove git;
+sudo yum -y install git2u-all;
 
-Echo "Creating carts.service"
+echo "Creating carts.service"
 
 sudo cat <<EOF > /etc/systemd/system/carts.service 
 [Unit]
@@ -16,6 +23,6 @@ ExecStop=/usr/bin/docker stop carts
 [Install]
 WantedBy=default.target
 EOF
-
+sudo systemctl daemon-reload;
 echo "Web for carts has been configured, you can check it on web:8081 "
 
