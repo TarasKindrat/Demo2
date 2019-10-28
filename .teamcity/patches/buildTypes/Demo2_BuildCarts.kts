a85859e7026c72610cef8c86a79f9c14c220236c
taras <taras@TeamCity>
2019-10-28 14:52:34 +0000

    TeamCity change in 'Demo2' project: 'Build_Carts' build configuration settings were updated

--- .teamcity/patches/buildTypes/Demo2_BuildCarts.kts
+++ .teamcity/patches/buildTypes/Demo2_BuildCarts.kts
@@ -49,6 +49,7 @@ create(RelativeId("Demo2"), BuildType({
         step {
             name = "Start carts service"
             type = "ssh-exec-runner"
+            enabled = false
             param("jetbrains.buildServer.deployer.username", "taras")
             param("jetbrains.buildServer.sshexec.command", "sudo systemctl start carts")
             param("jetbrains.buildServer.deployer.targetUrl", "web")
