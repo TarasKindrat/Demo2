5586b625dfed8398af0660b13b8127fa80bc866d
Taras <kindrat5@gmail.com>
2019-10-28 23:09:06 +0200

    add newest git installation

--- files/teamcity_install.sh
+++ files/teamcity_install.sh
@@ -8,6 +8,13 @@ echo $(ansible --version);
 # Install java
 echo "Installing wget and Java opedjdk-devel";
 sudo yum -y install java-1.8.0-openjdk-devel;
+echo "install latest git versin"
+sudo yum -y groupinstall "Development Tools";
+sudo yum -y install gettext-devel openssl-devel perl-CPAN perl-devel zlib-devel;
+sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm;
+# remove old version if exist
+sudo yum -y remove git;
+sudo yum -y install git2u-all;
 echo "Expotr HOME-variables for jre and java end new PATH"
 #export $(/usr/bin/env java -XshowSettings:properties -version 2>&1 | grep "java.home" | sed -e 's/java.home/JRE_HOME/;s/ //g;')
 #export $(/usr/bin/env java -XshowSettings:properties -version 2>&1 | grep "java.home" | sed -e 's/java.home/JAVA_HOME/;s/ //g;'| rev | cut -c 5- | rev)
