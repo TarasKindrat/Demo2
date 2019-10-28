9a8ed46810c13f3408dc56c1d825a72181a4e937
taras <taras@TeamCity>
2019-10-28 16:41:52 +0000

    TeamCity change in 'Demo2' project: runners of 'Build_Carts' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_BuildCarts.kts
+++ .teamcity/patches/buildTypes/Demo2_BuildCarts.kts
@@ -64,7 +64,7 @@ create(RelativeId("Demo2"), BuildType({
             workingDir = "/home/taras/carts"
             scriptContent = """
                 #docker build uri#ref:dir
-                docker build https://github.com/TarasKindrat/Demo2.git#terraformInstances:Carts_Dockerfile -t carts_image .
+                docker build -t https://github.com/TarasKindrat/Demo2.git#terraformInstances:Carts_Dockerfile carts_image .
             """.trimIndent()
         }
     }
