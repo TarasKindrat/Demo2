078d141612a07a5387e72a0a4281edb0ead3e1ca
taras <taras@TeamCity>
2019-10-28 12:45:35 +0000

    TeamCity change in 'Demo2' project: VCS roots of 'Deploy_container' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_DeployContainer.kts
+++ .teamcity/patches/buildTypes/Demo2_DeployContainer.kts
@@ -12,5 +12,9 @@ in the project with id = 'Demo2', and delete the patch script.
 create(RelativeId("Demo2"), BuildType({
     id("Demo2_DeployContainer")
     name = "Deploy_container"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
 }))
 
