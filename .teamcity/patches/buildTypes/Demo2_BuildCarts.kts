6f7ff858d9229e7d03092cdc047aae4e7d3fbcd2
taras <taras@TeamCity>
2019-10-29 22:17:01 +0000

    TeamCity change in 'Demo2' project: 'Build_Carts' build configuration settings were updated

--- .teamcity/patches/buildTypes/Demo2_BuildCarts.kts
+++ .teamcity/patches/buildTypes/Demo2_BuildCarts.kts
@@ -23,7 +23,6 @@ create(RelativeId("Demo2"), BuildType({
 
     steps {
         maven {
-            enabled = false
             goals = "clean test package"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
             jdkHome = "/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.232.b09-0.el7_7.x86_64/jre"
