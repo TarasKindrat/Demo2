accd0d2be28262347b816f6f3fbc25e8f51293b4
taras <taras@TeamCity>
2019-10-29 20:01:04 +0000

    TeamCity change in 'Demo2' project: runners of 'Create overlay network' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_CreateOverlayNetwork.kts
+++ .teamcity/patches/buildTypes/Demo2_CreateOverlayNetwork.kts
@@ -27,7 +27,7 @@ create(RelativeId("Demo2"), BuildType({
                 #self_ip=${'$'}(ifconfig | grep 'inet 10' | cut -d' ' -f10);
                 #swarm_token=${'$'}(docker swarm init --advertise-addr=${'$'}self_ip | awk 'NR==3{print ${'$'}0}'| tr -d 'docker ');
                 
-                swarm_token=${'$'}(docker swarm init | awk 'NR==3{print ${'$'}0}';
+                swarm_token=${'$'}(docker swarm init | awk 'NR==3{print ${'$'}0}');
                 echo "Swarm token is ${'$'}swarm_token"
                 # Set teamcity environment variable
                 echo "##teamcity[setParameter name='env.SWARM_TOKEN' value='${'$'}swarm_token']"
