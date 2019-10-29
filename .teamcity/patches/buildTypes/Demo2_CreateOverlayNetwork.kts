af16f13d74c5ebacce5d5e90bc5586daebfda1e7
taras <taras@TeamCity>
2019-10-29 20:42:54 +0000

    TeamCity change in 'Demo2' project: 'Create overlay network' build configuration settings were updated

--- .teamcity/patches/buildTypes/Demo2_CreateOverlayNetwork.kts
+++ .teamcity/patches/buildTypes/Demo2_CreateOverlayNetwork.kts
@@ -42,7 +42,6 @@ create(RelativeId("Demo2"), BuildType({
         step {
             name = "Swarm for overlay on mongo-db"
             type = "ssh-exec-runner"
-            enabled = false
             param("jetbrains.buildServer.deployer.username", "taras")
             param("jetbrains.buildServer.sshexec.command", """
                 # Join to swarm
