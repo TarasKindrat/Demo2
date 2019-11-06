5b198833669c7e1a5579662f8e9854d05a3ed1a8
taras <taras@TeamCity>
2019-11-06 22:17:22 +0000

    TeamCity change in 'Demo2' project: runners of 'Deploy databace's containers' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_DeployContainer.kts
+++ .teamcity/patches/buildTypes/Demo2_DeployContainer.kts
@@ -79,9 +79,9 @@ create(RelativeId("Demo2"), BuildType({
             scriptContent = """
                 git clone https://github.com/TarasKindrat/user.git;
                 docker build -f user/docker/user-db/Dockerfile user/docker/user-db/ -t user-db_image;
-                docker tag user-db_image gcr.io/demo2-256511/user-db_image::%build.number%;
-                docker push gcr.io/demo2-256511/user-db_image:%build.number%;
-                docker push gcr.io/demo2-256511/user-db_image:latest;
+                docker tag user-db_image gcr.io/demo2-256511/user-db_image:%build.number%;
+                docker push eu.gcr.io/demo2-256511/user-db_image:%build.number%;
+                docker push eu.gcr.io/demo2-256511/user-db_image:latest;
             """.trimIndent()
         }
     }
