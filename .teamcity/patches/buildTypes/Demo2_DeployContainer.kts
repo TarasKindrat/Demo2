16608cdfadf6deba56fac57e3dc2d623c357d554
taras <taras@TeamCity>
2019-11-06 21:17:52 +0000

    TeamCity change in 'Demo2' project: general settings of 'Deploy databace's containers' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_DeployContainer.kts
+++ .teamcity/patches/buildTypes/Demo2_DeployContainer.kts
@@ -14,7 +14,7 @@ create(RelativeId("Demo2"), BuildType({
     id("Demo2_DeployContainer")
     name = "Deploy databace's containers"
 
-    buildNumberPattern = "0.1%build.counter%"
+    buildNumberPattern = "0.0%build.counter%"
 
     vcs {
         root(DslContext.settingsRoot)
