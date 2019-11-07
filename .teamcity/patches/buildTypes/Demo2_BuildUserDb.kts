95763e4493c8afbb346625ed9c0e464b44aa6007
taras <taras@TeamCity>
2019-11-07 11:10:45 +0000

    TeamCity change in 'Demo2' project: general settings of 'Build user-db' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_BuildUserDb.kts
+++ .teamcity/patches/buildTypes/Demo2_BuildUserDb.kts
@@ -15,6 +15,8 @@ create(RelativeId("Demo2"), BuildType({
     id("Demo2_BuildUserDb")
     name = "Build user-db"
 
+    buildNumberPattern = "0.0%build.counter%"
+
     vcs {
         root(RelativeId("Demo2_UserGit"))
     }
