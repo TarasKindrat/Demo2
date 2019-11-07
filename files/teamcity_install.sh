0c66355501e3a925023968b69ee884072e51c725
Taras <kindrat5@gmail.com>
2019-11-07 11:43:36 +0200

    add installation google-cloud-sdk

--- files/teamcity_install.sh
+++ files/teamcity_install.sh
@@ -63,3 +63,15 @@ echo "statring Teamcity";
 sudo systemctl enable teamcity;
 sudo systemctl start teamcity;
 echo "Teamcity  are ready to use!"
+echo "Install Google cloud-SDK"
+sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-sdk]
+name=Google Cloud SDK
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+sudo yum -y install google-cloud-sdk
