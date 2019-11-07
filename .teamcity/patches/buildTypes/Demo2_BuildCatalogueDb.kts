2ef821e74412c69da775744ed7411f80eee505ec
taras <taras@TeamCity>
2019-11-07 19:55:17 +0000

    TeamCity change in 'Demo2' project: 'Catalogue' VCS root was created

--- .teamcity/patches/buildTypes/Demo2_BuildCatalogueDb.kts
+++ .teamcity/patches/buildTypes/Demo2_BuildCatalogueDb.kts
@@ -12,5 +12,9 @@ in the project with id = 'Demo2', and delete the patch script.
 create(RelativeId("Demo2"), BuildType({
     id("Demo2_BuildCatalogueDb")
     name = "Build catalogue-db"
+
+    vcs {
+        root(RelativeId("Demo2_Catalogue"))
+    }
 }))
 
