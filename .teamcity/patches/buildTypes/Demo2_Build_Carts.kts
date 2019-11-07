47cfd96ebeef53997a3adc16e7a16ed24d639987
Taras <kindrat5@gmail.com>
2019-11-07 22:52:08 +0200

    Update Demo2_Build_Carts.kts

--- .teamcity/patches/buildTypes/Demo2_Build_Carts.kts
+++ .teamcity/patches/buildTypes/Demo2_Build_Carts.kts
@@ -26,6 +26,21 @@ create(RelativeId("Demo2"), BuildType({
                 docker tag carts_image gcr.io/demo2-256511/carts_image:latest;
             """.trimIndent()
         }
+        script {
+            name = "Push images to Container Registry"
+            scriptContent = """
+                docker push gcr.io/demo2-256511/carts_image:%build.number%;
+                docker push gcr.io/demo2-256511/carts_image:latest;
+            """.trimIndent()
+        }
+        script {
+            name = "Delete images from build server"
+            scriptContent = """
+                docker rmi carts_image:latest;
+                docker rmi gcr.io/demo2-256511/carts_image:%build.number%;
+                docker rmi gcr.io/demo2-256511/carts_image:latest;
+            """.trimIndent()
+        }
     }
 }))
 
