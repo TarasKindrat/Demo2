97d3eca31832e66fad438e9d940d2bad13015062
taras <taras@TeamCity>
2019-11-07 10:13:45 +0000

    TeamCity change in 'Demo2' project: runners of 'Build user-db' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_BuildUserDb.kts
+++ .teamcity/patches/buildTypes/Demo2_BuildUserDb.kts
@@ -38,6 +38,14 @@ create(RelativeId("Demo2"), BuildType({
                 docker push gcr.io/demo2-256511/user-db_image:latest;
             """.trimIndent()
         }
+        script {
+            name = "Delete images"
+            scriptContent = """
+                docker rmi user-db_image:latest;
+                docker rmi gcr.io/demo2-256511/user-db_image:%build.number%;
+                docker rmi gcr.io/demo2-256511/user-db_image:latest;
+            """.trimIndent()
+        }
     }
 
     triggers {
