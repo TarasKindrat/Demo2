c509b182534a003f8d3b329aa3319a034f123c07
taras <taras@TeamCity>
2019-11-07 20:31:44 +0000

    TeamCity change in 'Demo2' project: 'Maven_create_carts.jar' build configuration settings were updated

--- .teamcity/patches/buildTypes/Demo2_BuildCarts.kts
+++ .teamcity/patches/buildTypes/Demo2_BuildCarts.kts
@@ -31,6 +31,7 @@ create(RelativeId("Demo2"), BuildType({
         step {
             name = "Stop carts container"
             type = "ssh-exec-runner"
+            enabled = false
             param("jetbrains.buildServer.deployer.username", "taras")
             param("jetbrains.buildServer.sshexec.command", """
                 if [ -n  ${'$'}(docker ps | awk '{print ${'$'}NF}' | grep -w carts) ]; then
