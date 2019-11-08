ad75057a2cbcd0ec55069b4db82a39e859eed5b2
taras <taras@TeamCity>
2019-11-08 16:05:54 +0000

    TeamCity change in 'Demo2' project: VCS roots of 'Build catalogue' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_BuildCatalogue.kts
+++ .teamcity/patches/buildTypes/Demo2_BuildCatalogue.kts
@@ -12,5 +12,9 @@ in the project with id = 'Demo2', and delete the patch script.
 create(RelativeId("Demo2"), BuildType({
     id("Demo2_BuildCatalogue")
     name = "Build catalogue"
+
+    vcs {
+        root(RelativeId("Demo2_Catalogue"))
+    }
 }))
 
