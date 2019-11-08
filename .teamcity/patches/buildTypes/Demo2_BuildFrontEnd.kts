7c2c922289126d57b8025c959e1810ce018a0636
taras <taras@TeamCity>
2019-11-08 19:57:35 +0000

    TeamCity change in 'Demo2' project: triggers of 'Build  front-end' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_BuildFrontEnd.kts
+++ .teamcity/patches/buildTypes/Demo2_BuildFrontEnd.kts
@@ -3,6 +3,7 @@ package patches.buildTypes
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -50,6 +51,12 @@ create(RelativeId("Demo2"), BuildType({
                 docker rmi gcr.io/demo2-256511/front-end_image:%build.number%;
                 docker rmi gcr.io/demo2-256511/front-end_image:latest;
             """.trimIndent()
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = ""
         }
     }
 }))
