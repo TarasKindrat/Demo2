2b882a9143aefb20d6bee8039644a2bca4fffed9
taras <taras@TeamCity>
2019-12-03 21:20:18 +0000

    TeamCity change in 'Demo2' project: VCS roots of 'setup ELK by Ansible' build configuration were updated

--- .teamcity/patches/buildTypes/Demo2_Setup_Elk_By_Ansible.kts
+++ .teamcity/patches/buildTypes/Demo2_Setup_Elk_By_Ansible.kts
@@ -12,5 +12,9 @@ in the project with id = 'Demo2', and delete the patch script.
 create(RelativeId("Demo2"), BuildType({
     id("Demo2_Setup_Elk_By_Ansible")
     name = "setup ELK by Ansible"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
 }))
 
